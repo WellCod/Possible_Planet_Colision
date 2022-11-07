1d5dbd2e2229428951862a038db994e1ea428cd8
WellCod <weslleygoncalves18@gmail.com>
2022-11-06 21:28:57 -0300

    comentarios sentry_service.js

--- src/sentry_service.js
+++ src/sentry_service.js
@@ -1,3 +1,4 @@
+//consome a API da NASA
 export async function getSentryObjects() {
     let response = await fetch("https://api.nasa.gov/neo/rest/v1/neo/sentry?api_key=DEMO_KEY")
     let result = await response.json()
